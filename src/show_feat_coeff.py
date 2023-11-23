3dc06ca32f04232d41ffdb4e1a13da955c70d356
RACHEL BOUWER <r.12.bouwer@gmail.com>
2023-11-22 22:50:33 -0800

    added coefficient interpretation function to src

---
+++ src/show_feat_coeff.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+
+def show_feat_coeff(pipe_obj, model_name, X):
+    """
+    Show the estimated feature coefficients for a learned regression model. 
+
+    Creates a new DataFrame with one column, whose index is the column names of the input 
+    array of examples, listing the estimated feature coefficients for the corresponding
+    features in the input array.
+
+    Parameters:
+    ----------
+    pipe_obj : Pipeline
+        The fitted input pipeline of transformers with a regression model estimator. Pipeline must be fitted to 
+        get coefficient values. 
+    model_name : str
+        The name of the regression model as per the Pipeline object named steps.
+    X : array-like shape (n_samples, n_features)
+        The input array-like shape containing the training data on which the input Pipeline object was fit.
+        The n_features dimension of the array-like shape will indicate the number of coefficient values to 
+        be returned corresponding to the column names for the n_features.
+
+    Returns:
+    -------
+    pandas.DataFrame
+        A DataFrame with one column:
+        - 'Coefficients': Lists the estimated feature coefficients for the input regression model.
+        whose index is the column names of the input array of test samples (X)
+        
+    Examples:
+    --------
+    >>> import pandas as pd
+    >>> from sklearn.pipeline import Pipeline
+    # Assume preprocessor object has been defined with appropriate feature transformations 
+    # Replace Lasso model with desired regression model (e.g. Ridge, LinearRegression)
+    >>> pipe = Pipeline([preprocessor, ('lasso', Lasso())]) 
+    >>> pipe.fit(X_train, y_train)
+    >>> results_df = show_feat_coeff(pipe, 'lasso', X_train)
+    >>> results_df
+    
+    Notes:
+    -----
+    This function uses the pandas library to perform grouping and counting
+    of class observations in the input DataFrame.
+
+    """
+    # Access .coef_ attribute of regression model 
+    coef_vals = pipe_obj.named_steps[model_name].coef_
+    coef_id = X.columns
+    # Report coefficient values and corresponding feature names in a DataFrame
+    result = pd.DataFrame(
+        data=coef_vals, index=coef_id, columns=["Coefficients"]
+        ).sort_values(by="Coefficients", ascending=False)
+    return result
