73ee47727d8037ca38e5b596bb77bc6ac03d4800
suumar <suumar@student.ubc.ca>
2023-11-22 23:42:52 -0800

    added doc string planned test for function

--- src/correlation_matrix.py
+++ src/correlation_matrix.py
@@ -1,20 +1,52 @@
 import pandas as pd
 
+def pearson_correlation_matrix(df, colormap='seismic'):
+    """Generate a styled Pearson correlation matrix plot for selected features.
 
-def pearson_correlation_matrix(df, colormap):
-    """
-    Generate and display a styled correlation matrix for the given DataFrame.
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The input DataFrame containing numeric columns for correlation analysis.
+    colormap : str, optional
+        The colormap for the background gradient styling of the correlation matrix.
+        Defaults to 'seismic'.
 
-    Parameters:
-    - df: DataFrame
+    Returns
+    -------
+    pandas.io.formats.style.Styler
+        A styled correlation matrix plot.
+
+    Notes
+    -----
+    - This function selects numeric columns without null values from the input DataFrame.
+    - Constant columns (columns with no variability) are excluded to avoid undefined correlations.
+    - The resulting correlation matrix is styled with a background gradient using the specified colormap input.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> from your_module import pearson_correlation_matrix
+
+    >>> # Load your DataFrame (e.g., train_df)
+    >>> train_df = pd.read_csv('your_data.csv')
+
+    >>> # Generate and display a Pearson correlation matrix plot
+    >>> pearson_matrix = pearson_correlation_matrix(train_df, colormap='viridis')
+    >>> display(pearson_matrix)
     """
     # Select numeric columns without null values
-    selected_features = df.select_dtypes(include="number").loc[
-        :, df.select_dtypes(include="number").nunique() > 1
+    selected_features = df.select_dtypes(include='number').loc[
+        :, df.select_dtypes(include='number').nunique() > 1
     ]
-    print("These are the selected features for the correlation matrix plot\n",selected_features.columns)
 
-    # Calculate the pearson correlation matrix
+     # Identify excluded columns
+    all_columns = set(df.select_dtypes(include='number').columns)
+    excluded_columns = list(all_columns - set(selected_features.columns))
+
+    # Print excluded columns
+    print("Excluded columns:\n", excluded_columns)
+
+    # Calculate the Pearson correlation matrix
     correlation_matrix = selected_features.corr()
 
     # Apply background gradient styling
@@ -22,5 +54,5 @@ def pearson_correlation_matrix(df, colormap):
         cmap=colormap
     )
 
-    # Display the styled correlation matrix
+    # Return the styled correlation matrix
     return correlation_matrix_style
