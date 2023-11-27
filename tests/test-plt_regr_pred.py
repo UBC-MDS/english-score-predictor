402094b8f906e4f92f0cd9ec749b27f19481eafd
farrandi <farrandi19@gmail.com>
2023-11-27 15:15:00 -0800

    fix test-plt_regr_pred.py to use unittest and change axes obj

--- tests/test-plt_regr_pred.py
+++ tests/test-plt_regr_pred.py
@@ -3,7 +3,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import pytest
 import sys
 import os
 from sklearn.pipeline import Pipeline
@@ -13,45 +12,49 @@ from sklearn.preprocessing import StandardScaler
 import unittest
 
 # Import the plt_regr_pred function from the src folder
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.plt_regr_pred import plt_regr_pred
 
-class TestPltRegrPred(unittest.TestCase):
 
+class TestPltRegrPred(unittest.TestCase):
     def setUp(self):
         # Create simple data for testing
-        pipe = Pipeline([('stdsclr', StandardScaler()), ('ridge', Ridge())])
+        pipe = Pipeline([("stdsclr", StandardScaler()), ("ridge", Ridge())])
         self.y = np.array([3, 6, 4, 4, 1, 6]).T
         self.X = pd.DataFrame(
-            data = np.array(
-                [[  1,    5,    4,   3,   0,    9],
-                [0.1,  0.3, -0.4,   0, 0.8, -0.9],
-                [100,  101,  230, 333, 305,  440]]
+            data=np.array(
+                [
+                    [1, 5, 4, 3, 0, 9],
+                    [0.1, 0.3, -0.4, 0, 0.8, -0.9],
+                    [100, 101, 230, 333, 305, 440],
+                ]
             ).T,
-            columns = ['feat1', 'feat2', 'feat1']
+            columns=["feat1", "feat2", "feat1"],
         )
         # Fit pipeline so that we can predict
         self.pipe_obj = pipe.fit(self.X, self.y)
 
     def test_plt_regr_pred_returns_axis(self):
         # Test for correct return type
-        result = plt_regr_pred(self.X, self.y, self.pipe_obj)
-        self.assertIsInstance(result, plt.Axes, "plt_regr_pred should return an Axes object")
+        _, ax = plt_regr_pred(self.X, self.y, self.pipe_obj)
+        self.assertIsInstance(
+            ax, plt.Axes, "plt_regr_pred should return an Axes object"
+        )
 
     def test_plt_regr_scatter_plot(self):
         # Test if scatter plot was created with same number of points as records in input data frame
-        result = plt_regr_pred(self.X, self.y, self.pipe_obj)
-        scatter_pts = result.collections
+        _, ax = plt_regr_pred(self.X, self.y, self.pipe_obj)
+        scatter_pts = ax.collections
         self.assertTrue(scatter_pts)
         self.assertEqual(len(scatter_pts[0].get_offsets()), len(self.y))
 
     def test_plt_regr_labels(self):
         # Test if scatter plot has correct labels
-        result = plt_regr_pred(self.X, self.y, self.pipe_obj)
-        self.assertEqual(result.get_xlabel(), "Actual Target")
-        self.assertEqual(result.get_ylabel(), "Predicted Target")
-        self.assertEqual(result.get_title(),"Actual vs. Predicted Target Values")
+        _, ax = plt_regr_pred(self.X, self.y, self.pipe_obj)
+        self.assertEqual(ax.get_xlabel(), "Actual Target")
+        self.assertEqual(ax.get_ylabel(), "Predicted Target")
+        self.assertEqual(ax.get_title(), "Actual vs. Predicted Target Values")
 
 
 if __name__ == "__main__":
-    pytest.main()
+    unittest.main()
