2ac5afcd99784a762f19fe625c30c21952cb76e1
Atabak-Alishiri <alishiriatabak@gmail.com>
2023-12-01 12:45:14 -0800

    the script now saves the sample data into a .zip file

--- src/english_score_get_data.py
+++ src/english_score_get_data.py
@@ -3,49 +3,51 @@ from sklearn.model_selection import train_test_split
 import click
 import os
 
+
 @click.command()
-@click.option('--sample_size', type=float)
-@click.option('--test_size', type=float)
-
-
-
-def main(sample_size=0.3, test_size=0.3):
+@click.option("--url", required=True, type=str, help="URL of the dataset to download.")
+@click.option(
+    "--output_folder_path",
+    required=True,
+    type=str,
+    help="Path to the output folder where files will be saved.",
+)
+def main(url, output_folder_path, sample_size=0.3, test_size=0.3):
     """
-    Main function for dataset sampling, splitting, and saving.
+    Downloads, samples, splits, and saves a dataset.
 
     This function downloads a dataset from a specified URL using pandas.
-    It samples a fraction of the dataset based on the provided sample size and saves 
-    this sampled dataset to a CSV file. It then splits the sampled dataset into 
-    training and testing subsets based on the provided test size, and saves these 
-    subsets to separate CSV files in the '../data/raw' directory. The directory 
-    is created if it does not exist.
+    It samples a fraction of the dataset (default: 30%) and saves this sampled dataset
+    to a ZIP file. It then splits the sampled dataset into training and testing subsets
+    (default test size: 30%), and saves these subsets to separate CSV files in the specified
+    directory. The directory is created if it does not exist.
 
     Parameters:
-    sample_size (float, optional): Fraction of the original dataset to sample. 
+    url (str): URL of the dataset to download.
+    output_folder_path (str): Path to the output folder where files will be saved.
+    sample_size (float, optional): Fraction of the original dataset to sample.
                                    Defaults to 0.3.
-    test_size (float, optional): Fraction of the sampled dataset to allocate 
+    test_size (float, optional): Fraction of the sampled dataset to allocate
                                  for the test set. Defaults to 0.3.
 
     Returns:
-    None: This function does not return anything but saves the sampled, training, 
+    None: This function does not return anything but saves the sampled, training,
           and testing datasets as CSV files.
     """
 
     # Ensure the directory exists
-    directory = "./data/raw"
+    directory = output_folder_path
     if not os.path.exists(directory):
         os.makedirs(directory)
 
     # Download and sample the dataset
-    dataset = pd.read_csv(
-        "https://osf.io/download/g72pq/", sep=",", on_bad_lines="skip", low_memory=False
-    )
+    dataset = pd.read_csv(url, sep=",", on_bad_lines="skip", low_memory=False)
     sampled_dataset = dataset.sample(frac=sample_size, random_state=123)
-    sampled_file_path = os.path.join(directory, "sampled_dataset.csv")
-    sampled_dataset.to_csv(sampled_file_path, index=False)
-    
+
     # Splitting the DataFrame
-    train_df, test_df = train_test_split(sampled_dataset, test_size=test_size, random_state=123)
+    train_df, test_df = train_test_split(
+        sampled_dataset, test_size=test_size, random_state=123
+    )
 
     # Save the training and testing datasets
     train_file_path = os.path.join(directory, "train_dataset.csv")
@@ -53,8 +55,11 @@ def main(sample_size=0.3, test_size=0.3):
     train_df.to_csv(train_file_path, index=False)
     test_df.to_csv(test_file_path, index=False)
 
+    # Save the sampled dataset directly to a ZIP file
+    zip_file_path = os.path.join(directory, "sampled_dataset.zip")
+    compression_opts = dict(method="zip", archive_name="sampled_dataset.csv")
+    sampled_dataset.to_csv(zip_file_path, index=False, compression=compression_opts)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
