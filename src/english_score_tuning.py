6ac3d837819c6731b6bd052ae5b2cb7e0e5aa001
farrandi <farrandi19@gmail.com>
2023-12-01 17:05:16 -0800

    Get preprocessor from pickle

--- src/english_score_tuning.py
+++ src/english_score_tuning.py
@@ -1,5 +1,6 @@
 import click
 import pandas as pd
+import pickle
 import sys
 from sklearn.compose import make_column_transformer
 from sklearn.impute import SimpleImputer
@@ -38,12 +39,12 @@ SCORING = {"RMSE": "neg_root_mean_squared_error", "R squared": "r2"}
     default="results/models/",
     help="Output directory, default is `results/models/`",
 )
-def main(
-    verbose,
-    train,
-    test,
-    output_dir,
-):
+@click.option(
+    "--preprocessor_path",
+    default="results/models/preprocessor/preprocessor.pkl",
+    help="Path to the preprocessor object (default: results/models/preprocessor/preprocessor.pkl)",
+)
+def main(verbose, train, test, output_dir, preprocessor_path):
     """Runs the analysis of the English Score Tuning."""
 
     if verbose:
@@ -53,7 +54,7 @@ def main(
 
     if verbose:
         click.echo("Getting and fitting the preprocessor...")
-    preprocessor = get_preprocessor()
+    preprocessor = get_preprocessor(preprocessor_path)
     preprocessor.fit(X_train)
 
     # Ridge Regression
@@ -173,160 +174,17 @@ def get_test_data(test):
     return X_test, y_test
 
 
-def get_preprocessor():
-    """Returns the preprocessor object from the script"""
+def get_preprocessor(path):
+    """Returns the preprocessor object from the path (pickle file)"""
+    with open(path, "rb") as f:
+        preprocessor = pickle.load(f)
 
-    # TODO @farrandi - Change it to actually take it from a file
-    numeric_feats = ["age", "Eng_start", "Eng_country_yrs", "Lived_Eng_per"]
+    return preprocessor
 
-    binary_feats = ["psychiatric"]
-    target = ["correct"]
 
-    drop_feats = [
-        "id",
-        "time",
-        "Unnamed: 0",
-        "tests",
-        "elogit",
-        "dyslexia",
-        "dictionary",
-        "already_participated",
-        "natlangs",
-        "primelangs",
-        "Can_region",
-        "Ir_region",
-        "US_region",
-        "UK_region",
-        "UK_constituency",
-        "gender",
-        "type",
-        "currcountry",
-        "countries",
-        "q1",
-        "q2",
-        "q3",
-        "q5",
-        "q6",
-        "q7",
-        "q9_1",
-        "q9_4",
-        "q10_2",
-        "q10_4",
-        "q11_3",
-        "q11_4",
-        "q12_1",
-        "q12_2",
-        "q12_4",
-        "q13_3",
-        "q13_4",
-        "q14_3",
-        "q14_4",
-        "q15_1",
-        "q15_2",
-        "q15_3",
-        "q16_3",
-        "q16_4",
-        "q17_1",
-        "q17_3",
-        "q17_4",
-        "q18_2",
-        "q18_3",
-        "q18_4",
-        "q19_1",
-        "q19_2",
-        "q19_3",
-        "q19_4",
-        "q20_1",
-        "q20_2",
-        "q20_3",
-        "q20_4",
-        "q21_1",
-        "q21_2",
-        "q21_3",
-        "q21_4",
-        "q22_1",
-        "q22_2",
-        "q22_3",
-        "q22_4",
-        "q23_3",
-        "q23_4",
-        "q24_1",
-        "q24_2",
-        "q24_3",
-        "q24_4",
-        "q25_1",
-        "q25_2",
-        "q25_3",
-        "q25_4",
-        "q26_1",
-        "q26_2",
-        "q26_3",
-        "q26_4",
-        "q27_1",
-        "q27_2",
-        "q27_3",
-        "q27_4",
-        "q28_1",
-        "q28_2",
-        "q29_1",
-        "q29_2",
-        "q29_3",
-        "q29_4",
-        "q30_1",
-        "q30_2",
-        "q30_3",
-        "q30_4",
-        "q31_1",
-        "q31_4",
-        "q32_5",
-        "q32_6",
-        "q32_8",
-        "q33_4",
-        "q33_5",
-        "q33_6",
-        "q33_7",
-        "q34_1",
-        "q34_2",
-        "q34_3",
-        "q34_4",
-        "q34_6",
-        "q34_8",
-        "q35_1",
-        "q35_2",
-        "q35_4",
-        "q35_5",
-        "q35_7",
-        "q35_8",
-    ]
-
-    binary_withNA = [
-        "house_Eng",
-        "nat_Eng",
-        "prime_Eng",
-    ]  # ["Ebonics", "house_Eng", "nat_Eng", "prime_Eng"]
-
-    categorical_education = ["education"]  # show 7 major categories and Others
-    categorical_feats = ["Eng_little", "speaker_cat"]
-    # categorical_countries = ['countries']
-
-    numeric_transformer = make_pipeline(
-        SimpleImputer(strategy="median"), StandardScaler()
-    )
-
-    categorical_transformer = make_pipeline(
-        SimpleImputer(strategy="constant", fill_value="little"),
-        OneHotEncoder(handle_unknown="ignore", sparse_output=False),
-    )
-
-    binary_NA_transformer = make_pipeline(
-        SimpleImputer(strategy="constant", fill_value=0),
-        OneHotEncoder(
-            handle_unknown="ignore", sparse_output=False, drop="if_binary", dtype=int
-        ),
-    )
-
-    # Defines the order for Education to be used in the OrdinalEncoder
-    education_order = [
+def map_to_other(df):
+    """helper function to map categories to others, used in the preprocessor"""
+    categories_list = [
         "Graduate Degree",
         "Some Graduate School",
         "Undergraduate Degree (3-5 years higher ed)",
@@ -337,26 +195,9 @@ def get_preprocessor():
         "Others",
     ]
 
-    # Define the custom function to map values to 'Other'
-    def map_to_other(df):
-        return (
-            df["education"].apply(lambda x: x if x in education_order else "Others")
-        ).to_frame()
-
-    # Create a transformer using FunctionTransformer
-    categorical_education_tranformer = make_pipeline(
-        FunctionTransformer(map_to_other, feature_names_out="one-to-one"),
-        OrdinalEncoder(categories=[education_order]),
-    )
-
-    return make_column_transformer(
-        ("drop", drop_feats),
-        (numeric_transformer, numeric_feats),
-        (categorical_transformer, categorical_feats),
-        ("passthrough", binary_feats),
-        (binary_NA_transformer, binary_withNA),
-        (categorical_education_tranformer, categorical_education),
-    )
+    return (
+        df["education"].apply(lambda x: x if x in categories_list else "Others")
+    ).to_frame()
 
 
 if __name__ == "__main__":
