bb5e1cd4f92e29b358a28ce8ca67d63dd07349e4
suumar <suumar@student.ubc.ca>
2023-12-01 21:22:20 -0800

    added correlation-matrix csv export

--- src/scripts/english_score_eda.py
+++ src/scripts/english_score_eda.py
@@ -12,12 +12,10 @@ from sklearn.preprocessing import (
     StandardScaler,
     FunctionTransformer,
 )
-
 import pickle
 from sklearn.impute import SimpleImputer
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import Pipeline, make_pipeline
-
 
 sys.path.append('src')
 
@@ -26,51 +24,63 @@ from helper.plot_histogram_with_exclusions import plot_histogram_with_exclusions
 
 
 @click.command()
-@click.option("--training-data", type=str, help="Path to training data ex: data/raw/")
+@click.option("--verbose", "-v", is_flag=True, help="Will print verbose messages.")
+@click.option("--training-data", type=str, 
+              default="data/raw/train_data.csv",
+              help="Path to training data ex: data/raw/")
 @click.option(
     "--plot-to",
     type=str,
+    default="results/figures/",
     help="Path to where all plots should be written ex: results/figures/",
 )
 @click.option(
     "--pickle-to",
     type=str,
+    default="results/models/preprocessor/",
     help="Path to where preprocessor should be written ex: results/models/preprocessor",
 )
-def main(training_data, plot_to, pickle_to):
+@click.option(
+    "--tables-to",
+    type=str,
+    default="results/tables/",
+    help="Path to where tables should be written ex: results/tables/",
+)
+def main(verbose,training_data, plot_to, pickle_to, tables_to):
+    """
+    Main function for data preprocessing and visualization.
+
+    Args:
+        training_data (str): Path to the training data CSV file.
+        plot_to (str): Path to save the generated plots.
+        pickle_to (str): Path to save the preprocessor object.
+        tables_to (str): Path to save the tables.
+
+    Returns:
+        None
+    """
     # Read the training data
     train_df = pd.read_csv(
         training_data, sep=",", on_bad_lines="skip", low_memory=False
     )
-    print(train_df.shape)
+    if verbose:
+        click.echo("Loaded Training Dataset...")
+    
 
     # Columns to be dropped
     drop_feats = [
-        "id",
-        "date",
-        "time",
-        "Unnamed: 0",
-        "tests",
-        "elogit",
-        "dyslexia",
-        "dictionary",
-        "already_participated",
-        "natlangs",
-        "primelangs",
-        "Can_region",
-        "Ir_region",
-        "US_region",
-        "UK_region",
-        "UK_constituency",
-        "gender",
-        "type",
-        "currcountry",
-        "countries",
+        "id", "date", "time", "Unnamed: 0", "tests", "elogit", "dyslexia",
+        "dictionary", "already_participated", "natlangs", "primelangs",
+        "Can_region", "Ir_region", "US_region", "UK_region", "UK_constituency",
+        "gender", "type", "currcountry", "countries",
     ]
 
     # Additional columns related to questions to be dropped
     question_columns_to_drop = [col for col in train_df.columns if col.startswith("q")]
     drop_feats += question_columns_to_drop
+
+    if verbose:
+        click.echo("Creating Numeric Feature Plots...")
 
     # Create the plots for numeric figures plotting
     plot_1 = plot_histogram_with_exclusions(train_df, columns_to_exclude=drop_feats)
@@ -78,6 +88,7 @@ def main(training_data, plot_to, pickle_to):
     # Specify the grid layout
     rows = 3
     cols = plot_1.size // rows
+
 
     # Create subplots
     for i, plot in enumerate(np.ravel(plot_1)):
@@ -106,6 +117,9 @@ def main(training_data, plot_to, pickle_to):
     # Save the education level plot
     education_level_plot.save(plot_to + "/education-level-fig.png", format="png")
 
+    if verbose:
+        click.echo("Creating Categorical Feature PLots...")
+
     # Categorical Feature Plot
     categorical_plot = (
         alt.Chart(train_df, title="Distribution of the Categorical features")
@@ -120,6 +134,7 @@ def main(training_data, plot_to, pickle_to):
 
     # Save the categorical plots
     categorical_plot.save(plot_to + "/feat-categoric-figs.png", format="png")
+    
 
     # Columns for the correlation matrix
     normal_cols = list(
@@ -129,10 +144,14 @@ def main(training_data, plot_to, pickle_to):
             + ["Unnamed: 0", "date", "time", "id"]
         )
     )
+    if verbose:
+        click.echo("Creating Correlation Matrix...")
 
     # Generate and save the Pearson correlation matrix
     cm = pearson_correlation_matrix(train_df[normal_cols], "seismic")
     dfi.export(cm, plot_to + "feat-correlation-matrix.png")
+
+    cm.data.to_csv(tables_to+"correlation-matrix.csv")
 
     # Final additional feature definitions
     numeric_feats = ["age", "Eng_start", "Eng_country_yrs", "Lived_Eng_per"]
@@ -140,9 +159,7 @@ def main(training_data, plot_to, pickle_to):
     binary_feats = ["psychiatric"]
     target = ["correct"]
     binary_withNA = [
-        "house_Eng",
-        "nat_Eng",
-        "prime_Eng",
+        "house_Eng", "nat_Eng", "prime_Eng",
     ]
 
     numeric_transformer = make_pipeline(
@@ -169,14 +186,10 @@ def main(training_data, plot_to, pickle_to):
 
     # Defines the order for Education to be used in the OrdinalEncoder
     education_order = [
-        "Graduate Degree",
-        "Some Graduate School",
-        "Undergraduate Degree (3-5 years higher ed)",
-        "Some Undergrad (higher ed)",
-        "High School Degree (12-13 years)",
+        "Graduate Degree", "Some Graduate School", "Undergraduate Degree (3-5 years higher ed)",
+        "Some Undergrad (higher ed)", "High School Degree (12-13 years)",
         "Haven't Finished High School (less than 13 years ed)",
-        "Didn't Finish High School (less than 13 years ed)",
-        "Others",
+        "Didn't Finish High School (less than 13 years ed)", "Others",
     ]
 
     # Create a transformer using FunctionTransformer
@@ -184,6 +197,9 @@ def main(training_data, plot_to, pickle_to):
         FunctionTransformer(map_to_other, feature_names_out="one-to-one"),
         OrdinalEncoder(categories=[education_order]),
     )
+
+    if verbose:
+        click.echo("Creating preprocessor...")
 
     preprocessor = make_column_transformer(
         ("drop", drop_feats),
@@ -197,7 +213,10 @@ def main(training_data, plot_to, pickle_to):
 
     # Save the preprocessor object using pickle
     with open(pickle_to + "preprocessor.pkl", "wb") as file:
-        pickle.dump(preprocessor, file)
+         pickle.dump(preprocessor, file)
+    if verbose:
+        click.echo("Finished Pickling the preprocessor...")
+
 
 
 # Define the custom function to map values to 'Other'
@@ -209,3 +228,5 @@ def map_to_other(df, categories_list):
 
 if __name__ == "__main__":
     main()
+    if verbose:
+        click.echo("Done!")
